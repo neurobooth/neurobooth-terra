38bb1efaa05eaadd54dcbf12a9a9336ea43d034b
Mainak Jas <mainakjas@gmail.com>
2021-05-06 17:51:22 -0400

    Infer datetime too

--- infer_schema.py
+++ infer_schema.py
@@ -6,6 +6,7 @@ import json
 
 import os.path as op
 import pandas as pd
+from pandas.api.types import infer_dtype
 
 data_dir = ('/Users/mainak/Dropbox (Partners HealthCare)/neurobooth_data/'
             'register/')
@@ -14,14 +15,26 @@ csv_fname = op.join(data_dir,
 
 # pandas to bigquery datatype mapping
 # https://cloud.google.com/bigquery/docs/reference/rest/v2/tables#tablefieldschema
-mapping = dict(float64='FLOAT', str='STRING', int64='INTEGER')
+mapping = dict(floating='float', integer='integer', string='string')
 
-df = pd.read_csv(csv_fname)
+df = pd.read_csv(csv_fname, parse_dates=True)
 dtypes = df.dtypes.to_dict()
+for column in df.columns:
+    dtypes[column] = infer_dtype(df[column])
+    dtypes[column] = mapping[dtypes[column]]
+    if dtypes[column] == 'string':
+        val = df[column].dropna().iloc[0]
+        try:
+            pd.Timestamp(val)
+        except:
+            dtypes[column] = 'timestamp'
+
+# Convert to fields that SchemaField expects
 for k, v in dtypes.items():
-    dtypes[k] = v.name
-    if v.name == 'object':
-        dtypes[k] = 'str'
-    dtypes[k] = mapping[dtypes[k]]
+    dtypes[k] = {
+        "name": v,
+        "type": v,
+        "mode": "REQUIRED"
+    }
 
 print(json.dumps(dtypes, indent=4, sort_keys=True))

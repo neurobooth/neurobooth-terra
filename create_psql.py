8ebca2af4461123dfd421b6344a1c515b3c99a64
Mainak Jas <mainakjas@gmail.com>
2021-06-28 00:37:15 -0400

    Start pushing a df

--- create_psql.py
+++ create_psql.py
@@ -1,20 +1,58 @@
-import psycopg2
+from functools import partial
 
-try:
-    connect_str = "dbname='neurobooth' user='neuroboother' host='localhost' " + \
-                  "password='neuroboothrocks'"
-    # use our connection values to establish a connection
-    conn = psycopg2.connect(connect_str)
-    cursor = conn.cursor()
-    # create a new table with a single column called "name"
-    cursor.execute("""CREATE TABLE tutorials (name char(40));""")
-    # run a SELECT statement - no data in there, but we can try it
-    cursor.execute("""SELECT * from tutorials""")
-    conn.commit() # <--- makes sure the change is shown in the database
-    rows = cursor.fetchall()
-    print(rows)
-    cursor.close()
-    conn.close()
-except Exception as e:
-    print("Uh oh, can't connect. Invalid dbname, user or password?")
-    print(e)
+import pandas as pd
+
+import psycopg2
+import psycopg2.extras as extras
+
+#### Some useful PSQL commands
+# pg_ctl -D /usr/local/var/postgres start  --> start server
+# psql mydatabasename
+
+#### Monkeypatch psycopg2 functions
+
+def safe_close(func):
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except Exception as e:
+            cursor.close()
+            raise Exception(e)
+    return wrapper
+
+@safe_close
+def execute(cursor, cmd, fetch=False):
+    cursor.execute(cmd)
+    cursor.commit()
+    if fetch:
+        return cursor.fetchall()
+
+@safe_close
+def execute_batch(cursor, cmd, tuples, page_size=100):
+    extras.execute_batch(cursor, cmd, tuples, page_size)
+    cursor.commit()
+
+def df_to_psql(cursor, df, table_id):
+    tuples = [tuple(x) for x in df.to_numpy()]
+    # Comma-separated dataframe columns
+    cols = ','.join(list(df.columns))
+    schema = ','.join(len(df.columns) * ['%s'])
+    cmd = f'INSERT INTO {table_id}({cols}) VALUES({schema})'
+    execute_batch(cursor, cmd, tuples)
+
+
+connect_str = ("dbname='neurobooth' user='neuroboother' host='localhost' "
+               "password='neuroboothrocks'")
+table_id = 'consent'
+csv_fname = ('/Users/mainak/Dropbox (Partners HealthCare)/neurobooth_data/'
+             'register/consent.csv')
+
+conn = psycopg2.connect(connect_str)
+cursor = conn.cursor()
+
+df = pd.read_csv(csv_fname)
+df = df.where(~df.isna(), None)
+df_to_psql(cursor, df, table_id)
+
+cursor.close()
+conn.close()

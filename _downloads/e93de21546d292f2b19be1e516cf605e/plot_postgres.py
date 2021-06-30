8d9cd412ffd048d0a46ef890e1be8b145c406084
Mainak Jas <mainakjas@gmail.com>
2021-06-30 16:11:57 -0400

    Make install

---
+++ _downloads/e93de21546d292f2b19be1e516cf605e/plot_postgres.py
@@ -0,0 +1,15 @@
+"""
+=============================================
+Create Postgres table using Neurobooth-terra.
+=============================================
+
+This example demonstrates how to create postgres table with neurobooth-terra.
+"""
+
+# Authors: Mainak Jas <mjas@harvard.mgh.edu>
+
+import os.path as op
+import tempfile
+
+###############################################################################
+# Let us import ``neurobooth-terra``.

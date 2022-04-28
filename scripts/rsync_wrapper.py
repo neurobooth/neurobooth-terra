1dc737bb9da9cb42789b584436919fbba2e35a2b
Mainak Jas <mainakjas@gmail.com>
2022-04-28 15:34:55 -0400

    Works?

--- scripts/rsync_wrapper.py
+++ scripts/rsync_wrapper.py
@@ -49,20 +49,23 @@ def write_file(sensor_file_table, db_table, fname, id=0):
         The row number of sensor_file_table to be used as primary key.
     """
     dir, fname = os.path.split(fp.name)
+    dir = os.path.join(dir, '')  # ensure trailing slash
     column_names = ['log_sensor_file_id', 'sensor_file_path']
     sensor_file_table.insert_rows(
         [(f'sensor_{id}', [fname])], cols=column_names)
 
     time_copied = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     column_names = ['log_sensor_file_id', 'src_dirname', 'fname',
-                    'time_copied', 'rsync_operation', 'is_deleted']
-    db_table.insert_rows([(f'sensor_{id}', dir, fname, time_copied, None, False)],
+                    'dest_dirname', 'time_copied', 'rsync_operation',
+                    'is_deleted']
+    db_table.insert_rows([(f'sensor_{id}', None, fname,
+                           dir, time_copied, None, False)],
                          cols=column_names)
 
 
 def transfer_files(src_dir, dest_dir, db_table, sensor_file_table):
     """Transfer files using rsync."""
-    out = subprocess.run(["rsync", src_dirname, dest_dirname, '-arzi',
+    out = subprocess.run(["rsync", src_dir, dest_dir, '-arzi',
                           "--out-format=%i %n%L %t"],
                          capture_output=True)
     if len(out.stderr) > 0:
@@ -81,15 +84,17 @@ def transfer_files(src_dir, dest_dir, db_table, sensor_file_table):
         if this_out.startswith('>f'):
             operation, fname, date_copied, time_copied = this_out.split(' ')
             # only write to table if files match with hash
-            if not _do_files_match(src_dirname, dest_dirname, fname):
+            if not _do_files_match(src_dir, dest_dir, fname):
                 continue
 
-            # _, fname = os.path.split(fname)
             df = sensor_file_table.query(
                 where=f"sensor_file_path @> ARRAY['{fname}']").reset_index()
             log_sensor_file_id = df.log_sensor_file_id[0]
 
-            db_rows.append((log_sensor_file_id, src_dirname, dest_dirname,
+            # ensure trailing slash (but don't provide to rsync)
+            dest_dir = os.path.join(dest_dir, '')
+            src_dir = os.path.join(src_dir, '')
+            db_rows.append((log_sensor_file_id, src_dir, dest_dir,
                             fname, f'{date_copied}_{time_copied}', operation,
                             False))
 
@@ -97,7 +102,7 @@ def transfer_files(src_dir, dest_dir, db_table, sensor_file_table):
     return db_rows
 
 
-def delete_files(db_table, target_dir, suitable_dest_dir=None, threshold=0.9,
+def delete_files(db_table, target_dir, suitable_dest_dir, threshold=0.9,
                  older_than=30):
     """Delete files if x% of disk is filled.
 
@@ -109,10 +114,8 @@ def delete_files(db_table, target_dir, suitable_dest_dir=None, threshold=0.9,
         is_deleted=True.
     target_dir : str
         The source directory path from which to delete files.
-    suitable_dest_dir : str | None
+    suitable_dest_dir : str
         The destination directory path where the files should have been copied.
-        If None, delete files regardless of which directory they have been
-        copied to.
     threshold : float
         A fraction between 0. and 1. If more than threshold fraction of the
         source directory is filled, these files will be deleted.
@@ -150,6 +153,10 @@ def delete_files(db_table, target_dir, suitable_dest_dir=None, threshold=0.9,
     Nas       who     True
     who       neo     False
     """
+    # ensure trailing slash
+    target_dir = os.path.join(target_dir, '')
+    suitable_dest_dir = os.path.join(suitable_dest_dir, '')
+
     stats = shutil.disk_usage(target_dir)
     if stats.used / stats.total < threshold:
         return
@@ -157,29 +164,36 @@ def delete_files(db_table, target_dir, suitable_dest_dir=None, threshold=0.9,
     where = f"dest_dirname='{target_dir}' "
     where += f"AND DATE_PART('day', AGE(current_timestamp, time_copied)) > {older_than} "
     where += "AND is_deleted=False"
-
     fnames_to_delete = db_table.query(where=where).fname
 
     if len(fnames_to_delete) == 0:
         print('No files to delete')
         return
 
-    fnames = ', '.join(fnames_to_delete.tolist())
+    fnames = [f"\'{fname}\'" for fname in fnames_to_delete.tolist()]
+    fnames = ', '.join(fnames)
     where = f"dest_dirname!='{suitable_dest_dir}' "
-    where += "AND is_deleted=False"  # just to be safe
-    where += "AND fname IN ({fnames})"
-    fnames_not_transferred = db_table.query().fname
-    print(f'The files {fnames_not_transferred} have been in target_dir '
-          f'for more than {older_than} days but have not been transferred '
-          'to {suitable_dest_dir}')
+    where += f"AND src_dirname IS NOT NULL "  # exclude write operations
+    where += "AND is_deleted=False "  # just to be safe
+    where += f"AND fname IN ({fnames})"
+    fnames_not_transferred = db_table.query(where=where).fname.tolist()
+    if len(fnames_not_transferred) > 0:
+        print(f'The files {fnames_not_transferred} have been in target_dir '
+              f'for more than {older_than} days but have not been transferred '
+              f'to suitable_dest_dir={suitable_dest_dir}')
     # TODO: output files that are in target_dir but not in log_file table
     # (e.g., research coordinator notes). Might require function to walk
     # over sub-folders.
 
     where = where.replace("dest_dirname!=", "dest_dirname=")
-    fnames_transferred = db_table.query().fname
+    fnames_transferred = db_table.query(where=where).fname.tolist()
+    fnames_transferred = [f"\'{fname}\'" for fname in fnames_transferred]
+    fnames_transferred = ', '.join(fnames_transferred)
+    if len(fnames_transferred) == 0:
+        raise ValueError('No files have been transferred to suitable_dest_dir')
+
     where = f"dest_dirname='{target_dir}' "
-    where += "AND fname IN ({fnames_transferred})"
+    where += f"AND fname IN ({fnames_transferred})"
     files_transferred_df = db_table.query(where=where)
 
     for operation_id, files_row in files_transferred_df.iterrows():
@@ -224,10 +238,9 @@ with psycopg2.connect(port='5432', host='localhost', **db_args) as conn:
                  foreign_key={'log_sensor_file_id': 'log_sensor_file'})
 
 
-src_dirname = mkdtemp() + os.sep
-# with TemporaryDirectory() as src_dirname:
+src_dirname = os.path.join(mkdtemp(), '')  # ensure path ends with /
 dest_dirname = mkdtemp()
-# with TemporaryDirectory() as dest_dirname:
+
 for id in range(5):
     with NamedTemporaryFile(dir=src_dirname, delete=False) as fp:
         fp.write(b'Hello world!')
@@ -242,5 +255,10 @@ with psycopg2.connect(port='5432', host='localhost', **db_args) as conn:
     db_table = Table('log_file', conn)
     db_rows = transfer_files(src_dirname, dest_dirname, db_table,
                              sensor_file_table)
-    delete_files(db_table, src_dirname, suitable_dest_dir=None,
-                 threshold=0.1)
+    delete_files(db_table, target_dir=src_dirname,
+                 suitable_dest_dir=dest_dirname,
+                 threshold=0.1, older_than=-1)
+
+# cleanup
+shutil.rmtree(src_dirname)
+shutil.rmtree(dest_dirname)

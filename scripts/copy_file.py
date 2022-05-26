94c87347b1682e06752b07eb5f6c57732deac1ff
Mainak Jas <mainakjas@gmail.com>
2022-05-26 14:57:33 -0400

    retrospective files

--- scripts/copy_file.py
+++ scripts/copy_file.py
@@ -18,3 +18,26 @@ with OptionalSSHTunnelForwarder(**ssh_args) as tunnel:
         sensor_file_table = Table('log_sensor_file', conn)
         db_table = Table('log_file', conn)
         copy_files(src_dir, dest_dir, db_table, sensor_file_table)
+
+# xdf and csv are not represented, other files did not get written due to
+# non-graceful exists.
+# Possible ways to handling retrospective entries added to log_sensor_file:
+# 1. Do not affect write_files assuming files still exist on NAS.
+#    It will simply copy over the new entries to log_file.
+# 2. However, retrospective entries should be added for copy operations.
+#    For example, if the data was copied from NAS to DrWho, the subsequent
+#    rsync operation will not produce an output for that file and hence
+#    it will not be added automatically to log_file.
+# 3. For deletion, there are two options:
+#    a. Retrospectively adding the data_verified by looking at file header (?)
+#    b. Use date_verified as current date and let the deletion happen x days
+#       from when the retrospective correction was done.
+#
+# class Foo:
+#    __enter__()
+#        pass
+#    __exit__():
+#        pass
+#
+# with Foo as foo:
+#    # do whatever

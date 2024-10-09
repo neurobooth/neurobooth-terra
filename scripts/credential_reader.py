66f5caf0f9db6e036f7c6ca0026adbffbd092067
sid4py <sid.plasmabhai@gmail.com>
2024-10-09 17:31:00 -0400

    credential reader reads configs from unified config repo

--- scripts/credential_reader.py
+++ scripts/credential_reader.py
@@ -1,23 +1,36 @@
+from typing import Optional
 import configparser as cp
 import os
 
 
-def read_db_secrets(db_secrets_file='.db.secrets.txt'):
+def read_db_secrets(config_fpath: Optional[str] = None):
 
     """
     Returns a dictionary of database credentials with keys:
     'database' for the name of the postgres database
     'user' for the pg username
     'password' for the pg user password
+    'host' for database host
 
-    The credential file is assumed to be in this folder. If it is not, replace the default argument
-    or override the default in the calling script
+    The credential file is assumed to be in at a location which
+    is defined in the TERRA_CONFIG_LOC environment variable
 
     """
 
-    path = os.path.join(os.path.dirname(__file__), db_secrets_file)
+    if config_fpath is None:
+        
+        db_secrets_fname='.db.secrets.txt'
+        
+        terra_config_file_location = os.environ.get('TERRA_CONFIG_LOC')
+        if terra_config_file_location is None:
+            raise Exception('got None when retreiving TERRA_CONFIG_LOC environment variable')
+        
+        config_fpath = os.path.join(terra_config_file_location, db_secrets_fname)
+        if not os.path.exists(config_fpath):
+            raise Exception(f'config file at {config_fpath} does not exist')
+
     config = cp.ConfigParser()
-    config.read(path)
+    config.read(config_fpath)
     db_creds = config['neurobooth.terra.db']
     credentials = {'database': db_creds['DB_Name'],
                    'user': db_creds['User'],

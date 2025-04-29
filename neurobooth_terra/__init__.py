a8c39c7490c23b80991f9417d3bed0708b2e1db0
Siddharth Patel <36140839+sid4py@users.noreply.github.com>
2025-04-28 20:01:49 -0400

    Import wearables redcap project (#58)
    * adding wearables project to config
    
    * handles error when unknown datatype found in redcap
    
    * add new redcap dataype slider to map_dtypes
    
    * fixing long standing problem with field annotations
    
    * adding the redcap wearables to postgres script
    
    * removing complete column check and renaming to wear_
    
    * marking version diff in redcap-py
    
    * adding explicit chmod perms to rsync
    
    * Update dataflow.py clarifying comment
    
    * adding ability to drop views in terra api
    
    * clarifying a comment
    
    * adding drop_view to __init__.py

--- neurobooth_terra/__init__.py
+++ neurobooth_terra/__init__.py
@@ -3,4 +3,4 @@
 __version__ = '0.1.dev0'
 
 from .postgres import (Table, create_table, drop_table, execute, list_tables,
-                       query, copy_table, list_views)
+                       query, copy_table, list_views, drop_view)
--- neurobooth_terra/dataflow.py
+++ neurobooth_terra/dataflow.py
@@ -257,8 +257,15 @@ def copy_files(src_dir, dest_dir, db_table, sensor_file_table):
     # and is_finished as False. In the next run, update_copystatus will
     # remove these lines from the db table as show_unfinished flag will be
     # set to true
+    # rsync sets directory and file permissions to 777, we want directory perms
+    # to be 750 and file perms to be 640 to protect data visibility. Therefore
+    # we add an explicit chmod flag with desired permissions. Note that the -a
+    # flag includes -p flag which stands for preserve permissions which is the
+    # reason why rsync copies 777 over from NAS. An explicit chmod rectifies
+    # this when provided.
     out = subprocess.run(["rsync", src_dir, dest_dir, '-a',
-                          "--out-format=%i %n%L %t"],
+                          "--out-format=%i %n%L %t",
+                          "--chmod=Du=rwx,Dg=rx,Do=,Fu=rw,Fg=r,Fo="],
                          capture_output=True)
 
     t2 = time.time()

cc23cc0436740c1c51677429df812ea3887c8f8a
sid4py <sid.plasmabhai@gmail.com>
2025-09-28 23:02:30 -0400

    added FA redcap project api key token to config

--- scripts/config.py
+++ scripts/config.py
@@ -17,9 +17,11 @@ dataflow_configs = reader.read_dataflow_configs()
 URL = 'https://redcap.partners.org/redcap/api/'
 API_KEY = os.environ.get('NEUROBOOTH_REDCAP_TOKEN')
 NB_WEAR_API_KEY = os.environ.get('WEARABLES_REDCAP_TOKEN')
+FA_API_KEY = os.environ.get('FA_REDCAP_PROJECT')
 
 if API_KEY is None:
     raise ValueError('Please define the environment variable NEUROBOOTH_REDCAP_TOKEN first')
 
 project = Project(URL, API_KEY, lazy=True)
 wearables_project = Project(URL, NB_WEAR_API_KEY, lazy=True)
+fa_project = Project(URL, FA_API_KEY, lazy=True)

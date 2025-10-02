d0c683ff04db58712c9e3b0a930b3fc8814c328a
Siddharth Patel <36140839+sid4py@users.noreply.github.com>
2025-10-02 15:36:04 -0400

    Config rework to accomodate fa redcap (#64)
    * added FA redcap project api key token to config
    
    * moved ssh args from config to credential reader
    
    * added redcap fa to pg script
    
    * changed comment
    
    * fixed env var name that I changes in env init file
    
    * changed name of data_dict csv files
    
    * redcap fa to postgres now works and populated database
    
    * added redcap fa subject script, doesn't work yet
    
    * refactored redcap subject to postgres script
    
    * send subject table to FA_study database as well
    
    * renamed files
    
    * removed backup file

--- scripts/config.py
+++ scripts/config.py
@@ -3,23 +3,21 @@ import os
 from redcap import Project
 import credential_reader as reader
 
-ssh_args = dict(
-        ssh_address_or_host='neurodoor.nmr.mgh.harvard.edu',
-        ssh_pkey='/local_mount/space/neurobooth/1/applications/config/id_rsa', # this is user sp1022's id_rsa
-        remote_bind_address=('192.168.100.1', 5432),
-        local_bind_address=('localhost', 6543)
-)
+secrets = reader.read_db_secrets()
 
-db_args = reader.read_db_secrets()
+db_args = secrets['db_args']
+ssh_args = secrets['ssh_args']
 
 dataflow_configs = reader.read_dataflow_configs()
 
 URL = 'https://redcap.partners.org/redcap/api/'
 API_KEY = os.environ.get('NEUROBOOTH_REDCAP_TOKEN')
 NB_WEAR_API_KEY = os.environ.get('WEARABLES_REDCAP_TOKEN')
+FA_API_KEY = os.environ.get('FA_REDCAP_TOKEN')
 
 if API_KEY is None:
     raise ValueError('Please define the environment variable NEUROBOOTH_REDCAP_TOKEN first')
 
 project = Project(URL, API_KEY, lazy=True)
 wearables_project = Project(URL, NB_WEAR_API_KEY, lazy=True)
+fa_project = Project(URL, FA_API_KEY, lazy=True)

74da97c2b5ab310f7c3c4481ba437e8f86775d74
Siddharth Patel <36140839+sid4py@users.noreply.github.com>
2026-02-11 20:07:17 -0500

    Add support for neurodoor2 in SSH tunneling (#74)

--- neurobooth_terra/fixes.py
+++ neurobooth_terra/fixes.py
@@ -7,24 +7,24 @@ class OptionalSSHTunnelForwarder(SSHTunnelForwarder):
     """SSH tunneling, skipped if already on neurodoor."""
 
     def __enter__(self):
-        if socket.gethostname() == 'neurodoor.nmr.mgh.harvard.edu':
+        if (socket.gethostname() == 'neurodoor.nmr.mgh.harvard.edu') or (socket.gethostname() == 'neurodoor2.nmr.mgh.harvard.edu'):
             return self
         return SSHTunnelForwarder.__enter__(self)
 
     @property
     def local_bind_port(self):
-        if socket.gethostname() == 'neurodoor.nmr.mgh.harvard.edu':
+        if socket.gethostname() == 'neurodoor.nmr.mgh.harvard.edu' or (socket.gethostname() == 'neurodoor2.nmr.mgh.harvard.edu'):
             return '5432'
         return super(OptionalSSHTunnelForwarder, self).local_bind_port
 
     @property
     def local_bind_host(self):
-        if socket.gethostname() == 'neurodoor.nmr.mgh.harvard.edu':
+        if socket.gethostname() == 'neurodoor.nmr.mgh.harvard.edu' or (socket.gethostname() == 'neurodoor2.nmr.mgh.harvard.edu'):
             return 'localhost'
         return super(OptionalSSHTunnelForwarder, self).local_bind_host
 
     def __exit__(self, exc_type, exc_value, traceback):
-        if socket.gethostname() == 'neurodoor.nmr.mgh.harvard.edu':
+        if socket.gethostname() == 'neurodoor.nmr.mgh.harvard.edu' or (socket.gethostname() == 'neurodoor2.nmr.mgh.harvard.edu'):
             return False
         return SSHTunnelForwarder.__exit__(self, exc_type, exc_value,
                                            traceback)
